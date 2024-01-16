1416ca3a3f5b30a9a3796fa710de492387c6e465
LGPinto <lgpintomkt@gmail.com>
2024-01-16 10:20:05 +0000

    change experiment to have p as the main parameter of analysis and remove r

--- MMLTM.py
+++ MMLTM.py
@@ -6,8 +6,9 @@ import matplotlib.pyplot as plt
 from numpy.random import choice
 from numpy.random import uniform
 from doepy import build
-import warnings
-#warnings.filterwarnings("ignore")
+import pandas as pd
+
+NUM_FLOOR=1e-4
 
 def logistic(x,r):
     return r*x*(1-x)
@@ -66,28 +67,20 @@ def random_mkt_mix(periods,granularity=1000):
         y=m*x+b
         y = MinMaxScaler(feature_range=(0,1)).fit_transform(y.reshape(-1, 1)).reshape(-1)
         mkt_mix.append(y)
-    mkt_mix=np.array(mkt_mix)
+    mkt_mix=np.array(mkt_mix)+NUM_FLOOR
     return mkt_mix
 
-def simulate_control(n,p,seed_set_p,mkt_mix,r=None,last=25):
+def simulate_control(n,p,seed_set_p,mkt_mix,r_promo=None,r_price=None,last=25):
     periods=mkt_mix.shape[1]
     G=nx.gnp_random_graph(n,p)
-    attributes=random_attributes(G)
+    attributes=random_attributes(G,seed_set_p)
     nx.set_node_attributes(G,attributes)
-    prods,prices,places,_=mkt_mix[0,:],mkt_mix[1,:],mkt_mix[2,:],mkt_mix[3,:]
-    
+    prods,prices,places,promos=mkt_mix[0,:],mkt_mix[1,:],mkt_mix[2,:],mkt_mix[3,:]
     nodes=[n for n in G.nodes]
     promo_cost=0
     previous_state=np.sum([attr['states'][0] for attr in attributes.values()])
     for t in range(periods):
-        #prod_control=logistic(previous_state/n,r)
-        #price_control=-logistic(previous_state/n,r)
-        #place_control=logistic(previous_state/n,r)
-        promo_control=logistic(previous_state/n,r)
-        promo_cost+=promo_control
-        #if t%10==0:
-        #    print("t="+str(t+1)+" adoption="+str(previous_state)+" curr_ad_exp="+str(round(promo_control,2))+" cum_ad_exp="+str(round(promo_cost,2)))
-        prod,price,place,promo=prods[t],prices[t],places[t],promo_control
+        prod,price,place,promo=prods[t],prices[t],places[t],promos[t]
         states=dict()
         for node in nodes:
             curr_state=[s for s in G.nodes()[node]['states']]
@@ -95,110 +88,107 @@ def simulate_control(n,p,seed_set_p,mkt_mix,r=None,last=25):
             states[node]=curr_state
         nx.set_node_attributes(G, states, "states")   
         previous_state=np.sum([st[-1] for st in nx.get_node_attributes(G,'states').values()])
-        if previous_state==0 and t<periods-last:
-            #print("t="+str(t+1)+" adoption="+str(previous_state)+" cum_ad_exp="+str(round(promo_cost,2)))
-            raise ValueError
     state_matrix=np.array(list(nx.get_node_attributes(G,"states").values()))
     state_matrix=np.array(state_matrix)/n
-    #print("t="+str(t+1)+" adoption="+str(previous_state)+" cum_ad_exp="+str(round(promo_cost,2)))
     return state_matrix
 
 def simulate_system(doe,n,seed_set_p,iterations,sims,last):
     print("Starting simulation with "+str(len(doe))+" exp. units each with "+str(sims)+" Monte Carlo simulations: a total of "+str(len(doe)*sims)+" runs...")
-    r_units=dict()
-    p_units=dict()
+    steadystates=[]
+    lyapunovs=[]
     for unit in doe:
-        steadystates,lyapunovs=[],[]
-        ri=unit[0]
-        p=unit[1]
-        #max_lyap=float('-inf')
+        #r_promo=unit[0]
+        #r_price=unit[1]
+        p=unit[0]
+        max_lyap=float('-inf')
+        x_isset=False
         for sim in range(sims):
             #within each Monte Carlo simulation
-            #we generate a random Watts-Stroggatz Graph with parameter p,n=500
+            #we generate a random Watts-Stroggatz Graph with parameter p,n=1000
             #we generate a random seed node sample always with seed_p=0.05
             #we generate a random marketing mix
             #we generate a random distribution of the thresholds uniformly
             #we generate a random distribution of the budgets uniformly
             mkt_mix=random_mkt_mix(iterations)
-            prod,price,place=mkt_mix[0,:],mkt_mix[1,:],mkt_mix[2,:]
+            prod,price,place,promo=mkt_mix[0,:],mkt_mix[1,:],mkt_mix[2,:],mkt_mix[3,:]
             prod_inc='increasing' if np.all(prod[1:] >= prod[:-1]) else 'decreasing'
             price_inc='increasing' if np.all(price[1:] >= price[:-1]) else 'decreasing'
             place_inc='increasing' if np.all(place[1:] >= place[:-1]) else 'decreasing'
+            promo_inc='increasing' if np.all(promo[1:] >= promo[:-1]) else 'decreasing'
             try:
-                simulation=simulate_control(n,p,seed_set_p,mkt_mix,ri,last)
+                simulation=simulate_control(n,p,seed_set_p,mkt_mix,None,None,last)
                 diffusion=simulation.sum(axis=0)
                 steadystate=diffusion[-last:]
             except ValueError:
                 steadystate=np.zeros((last))
             lyap=nolds.lyap_e(steadystate)[0]
-            if lyap != float('inf'): #check if lyapunov exponent is finite
-                steadystates.append(steadystate)
-                lyapunovs.append(lyap)
-                r_units[ri]=(steadystates,lyapunovs)
-                p_units[p]=(steadystates,lyapunovs)
-                print("r="+str(round(ri,2))+
-                      ", p="+str(round(p,2))+
+            if lyap !=float('inf') and lyap>max_lyap:
+                max_lyap=lyap
+                x=steadystate
+                x_isset=True
+            if lyap != float('inf') and max_lyap != float('-inf'): #check if lyapunov exponent is finite
+                print(
+                      "p="+str(round(p,2))+
                       ", sim="+str(sim+1)+
                       ", prod_qual="+prod_inc+
                       ", price_level="+price_inc+
                       ", dist_intensity="+place_inc+
-                      ", run. avg lyapunov="+str(round(np.mean(lyapunovs),2))
+                      ", promo_exp="+promo_inc+
+                      ", run. max lyapunov="+str(round(np.max([lyap,max_lyap]),2))
                       )
             else:
-                print("r="+str(round(ri,2))+
-                      ", p="+str(round(p,2))+
+                print(
+                      "p="+str(round(p,2))+
                       ", sim="+str(sim+1)+
                       ", prod_qual="+prod_inc+
                       ", price_level="+price_inc+
                       ", dist_intensity="+place_inc+
+                      ", promo_exp="+promo_inc+
                       ", infinite lyapunov"
                       )
-            
-    r_steadystate_agg=[unit[0] for r,unit in zip(r_units.keys(),r_units.values())]
-    r_lyapunov_agg=[np.mean(unit[1]) for r,unit in zip(r_units.keys(),r_units.values())]
-    p_steadystate_agg=[unit[0] for p,unit in zip(p_units.keys(),p_units.values())]
-    p_lyapunov_agg=[np.mean(unit[1]) for p,unit in zip(p_units.keys(),p_units.values())]
-    return np.array(r_steadystate_agg),np.array(r_lyapunov_agg),np.array(p_steadystate_agg),np.array(p_lyapunov_agg)
+        if x_isset:
+            steadystates.append((unit[0],unit[1],x))
+            lyapunovs.append((unit[0],unit[1],max_lyap))
+    return np.array(steadystates),np.array(lyapunovs)
 
-n=500           #graph network size
+n=1000          #graph network size
 seed_set_p=0.05 #seed set size
-iterations=150  #diffusion iterations
+iterations=1000  #diffusion iterations
 simulations=20  #Monte Carlo simulations within each DoE unit
-last=25         #steadystate size
-r_levels=100    #r factor levels
-p_levels=3      #p factor levels
-r = np.linspace(2.5, 4.0, r_levels)
-p = np.linspace(0.01, 0.3, p_levels)
-
-#total of 400*20=8000 simulations
+last=100         #steadystate size
+p_levels=100      #p factor levels
+p = np.linspace(0.01, 0.05, p_levels)
 
 doe=build.full_fact({
-     'r':r,
      'p':p
      }).values.tolist()
 
-steadystates,lyapunovs,p_st,p_lyap = simulate_system(doe,n,seed_set_p,iterations,simulations,last)
+steadystates,lyapunovs = simulate_system(doe,n,seed_set_p,iterations,simulations,last)
+
+rows=steadystates.shape[0]
+data=pd.DataFrame(np.concatenate([steadystates,lyapunovs[:,2].reshape(rows,1)],axis=1),columns=['r','p','steadystate','lyapunov'])
+data=data.groupby(['p']).agg({'lyapunov':'max','steadystate':'first'})
 
 #Plots
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 9),
                                sharex=True)
-lyapunov=lyapunovs
-x=steadystates
-ax1.plot(r, x, ',k', alpha=.25)
+lyapunov=data['lyapunov'].values
+x=np.array([np.array(xi) for xi in data['steadystate'].values])
+ax1.plot(p, x, ',k', alpha=.25)
 ax1.set_xlim(2.5, 4)
 ax1.set_title("Bifurcation diagram")
 
 # We display the Lyapunov exponent.
 # Horizontal line.
-ax2.axhline(0, color='k', lw=.5, alpha=.8)
+ax2.axhline(0, color='k', lw=.5, alpha=.3)
 # Negative Lyapunov exponent.
-ax2.plot(r[lyapunov < 0],
+ax2.plot(p[lyapunov < 0],
          lyapunov[lyapunov < 0] / last,
-         '.k', alpha=.8, ms=.5)
+         '.k', alpha=.3, ms=.5)
 # Positive Lyapunov exponent.
-ax2.plot(r[lyapunov >= 0],
+ax2.plot(p[lyapunov >= 0],
          lyapunov[lyapunov >= 0] / last,
-         '.r', alpha=.8, ms=.5)
+         '.r', alpha=.3, ms=.5)
 ax2.set_xlim(2.5, 4)
 ax2.set_ylim(-0.005, 0.015)
 ax2.set_title("Lyapunov exponent")
